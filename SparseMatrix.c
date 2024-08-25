f1ba9fb03d747821e3a8fec7a84c7bf88d45b031
SubhajitAdhikary2005 <subhajitadhikary2005123@gmail.com>
2024-08-25 19:24:48 +0530

    Add files via upload
    Check whether a matrix is sparse Matrix or not.

---
+++ SparseMatrix.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int SparseMatrix(int r,int c, int mat[100][100])
+{
+   int i,j,k,count=0;
+   for(i=0;i<r;i++)
+   {
+       for(j=0;j<c;j++)
+       {
+           if(mat[i][j]==0)
+           { 
+               count=count+1;
+           }
+       }
+   }       
+   if(count>(r*c)/2)
+   {
+        printf("This is a Sparse Matrix");
+   }
+   else
+   {  
+         printf("This is not a Sparse Matrix ");
+   }
+
+}
+int main()
+{
+    int i,j,r,c,mat[100][100];
+    printf("Enter number of rows: ");
+    scanf("%d",&r);
+    printf("Enter number of columns: ");
+    scanf("%d",&c);
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<c;j++)
+       {
+          printf("Enter (%d,%d) element:",i,j);
+          scanf("%d",&mat[i][j]);
+       }
+    }
+    printf("Matrix is:\n");
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<c;j++)
+       {
+          printf("%d ",mat[i][j]);
+       }
+       printf("\n");
+    }
+    SparseMatrix(r,c,mat);
+    return 0;
+}
