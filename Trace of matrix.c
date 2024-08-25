1f736c2c3d3636c07c72f2ba9f9bbc6ae09fcd67
SubhajitAdhikary2005 <subhajitadhikary2005123@gmail.com>
2024-08-25 20:42:14 +0530

    Add files via upload
    Trace of a matrix

---
+++ Trace of matrix.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int SparseMatrix(int r,int c, int mat[100][100])
+{
+   int i,j,k,trace=0;
+   if(r==c)
+   {
+      for(i=0;i<r;i++)
+      {
+          for(j=0;j<c;j++)
+          {
+             if(i==j)
+             { 
+                 trace=trace+mat[i][j];
+             }
+          }
+      }    
+   printf("Trace of matrix is: %d",trace);
+   }
+   else
+   {
+      printf("Rows and columns no. are not equal,it is not a Square Matrix. So, Trace of matrix is not possible");   
+   }
+
+}
+int main()
+{
+    int i,j,r,c,mat[100][100];
+    printf("Enter number of rows: ");
+    scanf("%d",&r);
+    printf("Enter number of columns: ");
+    scanf("%d",&c);
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<c;j++)
+       {
+          printf("Enter (%d,%d) element:",i,j);
+          scanf("%d",&mat[i][j]);
+       }
+    }
+    printf("Matrix is:\n");
+    for(i=0;i<r;i++)
+    {
+       for(j=0;j<c;j++)
+       {
+          printf("%d ",mat[i][j]);
+       }
+       printf("\n");
+    }
+    SparseMatrix(r,c,mat);
+    return 0;
+}
